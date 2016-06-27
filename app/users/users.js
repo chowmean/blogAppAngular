29fe974f33b1126c20d75ad17f3f960dd04ae019
Gaurav Yadav <gaurav.dev.iiitm@gmail.com>
2016-06-27 23:43:33 +0530

    adding users

--- app/users/users.js
+++ app/users/users.js
@@ -9,6 +9,57 @@ angular.module('myApp.dashboard', ['ngRoute'])
         });
     }])
 
-    .controller('usersCtrl', [function() {
+    .controller('usersCtrl', function($scope,$http,$window) {
 
-    }]);
+
+        $scope.getUsers=function() {
+            $http({
+                    method: 'GET',
+                    url: baseUrl + '/accounts/users',
+                    headers: {'token': $window.localStorage.getItem('tokenData')}
+                }
+            ).success(function (data) {
+                $scope.userData = data;
+                console.log($scope.userData)
+            })
+                .error(function (data) {
+                    console.log('error');
+                });
+
+        }
+
+        $scope.getUsers();
+
+        $scope.activate=function(userId)
+        {
+            $http({
+                method: 'POST',
+                url: baseUrl + '/accounts/activate/'+userId,
+                headers: {'token': $window.localStorage.getItem('tokenData')},
+                data: $scope.blogData
+            }).success(function(data){
+                console.log(data);
+                $scope.getUsers();
+
+            }).error(function(data){
+
+            })
+        }
+
+        $scope.deactivate=function(userId)
+        {
+            $http({
+                method: 'DELETE',
+                url: baseUrl + '/accounts/deactivate/'+userId,
+                headers: {'token': $window.localStorage.getItem('tokenData')},
+                data: $scope.blogData
+            }).success(function(data){
+                console.log(data);
+                $scope.getUsers();
+
+            }).error(function(data){
+
+            })
+
+        }
+    });
